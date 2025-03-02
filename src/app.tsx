08b6fede89b0f3e05d617407c4a8c1ede36a49cb
NMC00 <nick@nmchivers.design>
2025-03-02 13:00:37 -0800

    Added scss support

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'preact/hooks'
-import './app.css'
+import './app.scss'
 
 export function App() {
   const [count, setCount] = useState(0)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import { viteSingleFile } from 'vite-plugin-singlefile'
+import sass from 'sass'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact(), viteSingleFile()],
+  css: {
+    preprocessorOptions: {
+      sass: {
+        implementation: sass,
+      },
+    },
+  },
+  build: {
+    target: 'esnext',
+  }
 })
