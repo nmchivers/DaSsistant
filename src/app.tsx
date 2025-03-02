07d8e2e675601ea852168c93fc42735395a0be43
NMC00 <nick@nmchivers.design>
2025-03-02 11:43:09 -0800

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'preact/hooks'
+import './app.css'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  function handleOnChange(e: Event) {
+    const target = e.target as HTMLInputElement;
+    setCount(parseInt(target.value));
+  }
+
+  function handleOnCreate() {
+    parent.postMessage({ pluginMessage: { type: 'create-shapes', count } }, '*')
+  }
+
+  function handleOnCancel() {
+    parent.postMessage({ pluginMessage: { type: 'cancel' } }, '*')
+  }
+    
+  return (
+    <>
+      <h2>Rectangle Creator in Preact</h2>
+      <p>Count: <input id="count" type="number" value={count} onChange={handleOnChange} /></p>
+      <button id="create" onClick={handleOnCreate}>Create</button>
+      <button id="cancel" onClick={handleOnCancel}>Cancel</button>
+    </>
+  )
+}
