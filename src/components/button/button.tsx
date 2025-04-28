7c039d3c84b068e12628f8b625edd158eef8b4db
Nick <nick@nmchivers.design>
2025-04-27 19:12:37 -0700

    Refactored the button component and added styling for outline and hollow buttons as well as added icon support to the buttons

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,5 +1,6 @@
 import React, { ButtonHTMLAttributes } from "preact/compat";
 import './button.scss';
+import Icon from "../icon/icon";
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
     text: string;
@@ -21,59 +22,61 @@ function Button({
     isLoading = false,
     ...props}: Props) {
 
-    if (addClasses === undefined) {
-        if (iconOnly) {
-            addClasses = " icon-only";
-        } else if (icon !== "") {
-            addClasses = " has-icon";
-        } else {
-            addClasses = "";
-        }
-    } else {
-        if (iconOnly) {
-            addClasses = " " + addClasses + " icon-only";
-        } else if (icon !== "") {
-            addClasses = " " +  addClasses + " has-icon";
-        } else {
-            addClasses = " " +  addClasses;
-        }
+    var classes = (addClasses === undefined ? "button-" + variant : addClasses + "button-" + variant);
+    if (iconOnly) {
+        classes = classes + "-icon-only";
     }
 
     if (isLoading) {
-        addClasses = addClasses + " is-loading";
+        classes = classes + " is-loading";
     }
 
-    if (variant === "filled") {
-        return (
-            <button className={"button-filled" + addClasses} type={type} name={name} title={text} {...props}>
-                {!iconOnly && text}
-                {icon && <i className={icon}></i>}
-                {/* {isLoading && <Loader size="small" color="neutral-100" />} */}
-            </button>
-        )
-    } else if (variant === "outline") {
-        return (
-            <button className={"button-outline" + addClasses} type={type} name={name} title={text} {...props}>
-                {!iconOnly && text}
-                {icon && <i className={icon}></i>}
-                {/* {isLoading && <Loader size="small" color="interactive-500" />} */}
-            </button>
-        )
-    } else if (variant === "hollow") {
-        return (
-            <button className={"button-hollow" + addClasses} type={type} name={name} title={text} {...props}>
-                {!iconOnly && text}
-                {icon && <i className={icon}></i>}
-            </button>
-        )
-    } else {
-        return (
-            <button className={"button-hollow-square" + addClasses} type={type} name={name} title={text} {...props}>
-                {!iconOnly && text}
-                {icon && <i className={icon}></i>}
-            </button>
-        )
-    } 
+    return (
+      <button
+        className={classes}
+        type={type}
+        name={name}
+        title={text}
+        {...props}
+      >
+        {!iconOnly && text}
+        {icon && <Icon iconName={icon} addClasses="button-icon" />}
+        {/* {isLoading && <Loader size="small" color="neutral-100" />} */}
+      </button>
+    );
+
+    // if (variant === "filled") {
+    //     return (
+    //         <button className={"button-filled" + addClasses} type={type} name={name} title={text} {...props}>
+    //             {!iconOnly && text}
+    //             {icon && <Icon iconName={icon} addClasses="button-icon"/>}
+    //             {/* {icon && <i className={icon}></i>} */}
+    //             {/* {isLoading && <Loader size="small" color="neutral-100" />} */}
+    //         </button>
+    //     )
+    // } else if (variant === "outline") {
+    //     return (
+    //         <button className={"button-outline" + addClasses} type={type} name={name} title={text} {...props}>
+    //             {!iconOnly && text}
+    //             {icon && <i className={icon}></i>}
+    //             {/* {isLoading && <Loader size="small" color="interactive-500" />} */}
+    //         </button>
+    //     )
+    // } else if (variant === "hollow") {
+    //     return (
+    //         <button className={"button-hollow" + addClasses} type={type} name={name} title={text} {...props}>
+    //             {!iconOnly && text}
+    //             {icon && <i className={icon}></i>}
+    //         </button>
+    //     )
+    // } else {
+    //     return (
+    //         <button className={"button-hollow-square" + addClasses} type={type} name={name} title={text} {...props}>
+    //             {!iconOnly && text}
+    //             {icon && <i className={icon}></i>}
+    //         </button>
+    //     )
+    // } 
 }
 
 export default Button;
