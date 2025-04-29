fd512d13c992199f7f6d1631063ba62e3c4107a2
Nick <nick@nmchivers.design>
2025-04-29 12:30:58 -0700

    Added support for figma controlled dark mode.

--- lib/code.ts
+++ lib/code.ts
@@ -7,7 +7,7 @@
 // full browser environment (See https://www.figma.com/plugin-docs/how-plugins-run).
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__, { width: 640, height: 917 });
+figma.showUI(__html__, {themeColors: true, width: 640, height: 917 });
 
 // Calls to "parent.postMessage" from within the HTML page will trigger this
 // callback. The callback will be passed the "pluginMessage" property of the
